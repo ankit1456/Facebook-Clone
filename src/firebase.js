4f11e75e2d95569cc02988540aa6517ec46147be
Ankit Tripathi <ankittripathi1456@email.com>
2021-05-17 13:10:42 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDmM9AvlzhPde6taYPADwbfV7eRC1SlKFY",
+  authDomain: "facebook-clone-3bdc7.firebaseapp.com",
+  projectId: "facebook-clone-3bdc7",
+  storageBucket: "facebook-clone-3bdc7.appspot.com",
+  messagingSenderId: "961523455626",
+  appId: "1:961523455626:web:f1ebe2e565f484f0903f73",
+  measurementId: "G-T7K47516BR",
+};
